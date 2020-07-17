047b0d59a95082f1a15148b3303d461fca858788
F2VILLE <42893337+F2VILLE@users.noreply.github.com>
2020-07-17 22:19:27 +0200

    Comment code
    📝I commented this very complex GO script

--- go_hello_world.go
+++ go_hello_world.go
@@ -1,7 +1,11 @@
+// Import main package
 package main
 
+// import "fmt" 
 import "fmt"
 
+// Start main function
 func main() {
+	// Print Hello World in the console
 	fmt.Println("Hello, world!")
 }
